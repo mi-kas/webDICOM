c428e0a03d432f0f3bc26c4827791257f7afdc33
mikas <michael.kaserer@hotmail.com>
2013-03-14 09:56:56 +0100

    Checkings for undefined values added.

--- js/CanvasPainter.js
+++ js/CanvasPainter.js
@@ -6,7 +6,7 @@ function CanvasPainter(canvasId) {
     this.ww;
     this.wc;
     this.scale = 1;
-    this.pan = [100, 0];
+    this.pan = [0, 0];
 }
 
 CanvasPainter.prototype.setFile = function(file) {
@@ -30,35 +30,17 @@ CanvasPainter.prototype.setPan = function(panx, pany) {
 CanvasPainter.prototype.drawImg = function() {
     this.canvas.height = this.currentFile.Rows;
     this.canvas.width = this.currentFile.Columns;
+    var lowestVisibleValue = this.currentFile.WindowCenter - this.currentFile.WindowWidth / 2.0;
+    var highestVisibleValue = this.currentFile.WindowCenter + this.currentFile.WindowWidth / 2.0;
 
     this.context.fillStyle = "rgb(0,0,0)";
     this.context.fillRect(0, 0, this.currentFile.Columns, this.currentFile.Rows);
     var imgData = this.context.createImageData(this.currentFile.Columns, this.currentFile.Rows);
     var pixelData = this.currentFile.PixelData;
 
-    if(typeof this.currentFile.WindowCenter !== 'undefined' && typeof this.currentFile.WindowWidth !== 'undefined') {
-        if($.isArray(this.currentFile.WindowCenter) && $.isArray(this.currentFile.WindowWidth)) {
-            var lowestVisibleValue = this.currentFile.WindowCenter[0] - this.currentFile.WindowWidth[0] / 2.0;
-            var highestVisibleValue = this.currentFile.WindowCenter[0] + this.currentFile.WindowWidth[0] / 2.0;
-
-        } else {
-            var lowestVisibleValue = this.currentFile.WindowCenter - this.currentFile.WindowWidth / 2.0;
-            var highestVisibleValue = this.currentFile.WindowCenter + this.currentFile.WindowWidth / 2.0;
-        }
-
-    } else {
-        var lowestVisibleValue = 85 - 171 / 2.0;
-        var highestVisibleValue = 85 + 171 / 2.0;
-    }
-
-
     for(var i = 0; i < imgData.data.length; i += 4) {
         var intensity = pixelData[(i / 4)];
-
-        if(typeof this.currentFile.RescaleSlope !== 'undefined' && typeof this.currentFile.RescaleIntercept !== 'undefined') {
-            intensity = intensity * this.currentFile.RescaleSlope + this.currentFile.RescaleIntercept;
-        }
-
+        intensity = intensity * this.currentFile.RescaleSlope + this.currentFile.RescaleIntercept;
         intensity = (intensity - lowestVisibleValue) / (highestVisibleValue - lowestVisibleValue);
         if(intensity < 0.0)
             intensity = 0.0;
@@ -70,20 +52,20 @@ CanvasPainter.prototype.drawImg = function() {
         imgData.data[i + 0] = intensity; // R
         imgData.data[i + 1] = intensity; // G
         imgData.data[i + 2] = intensity; // B
-        imgData.data[i + 3] = 255; // alpha
+        imgData.data[i + 3] = 255;       // alpha
     }
 
     var ratio = this.currentFile.Columns / this.currentFile.Rows;
-    var targetWidth = this.currentFile.Rows * this.scale * ratio;
-    var targetHeight = this.currentFile.Columns * this.scale * ratio;
+    var targetWidth = ratio * this.scale * this.currentFile.Rows;
+    var targetHeight = ratio * this.scale * this.currentFile.Columns;
     var xOffset = (this.canvas.width - targetWidth) / 2 + this.pan[0];
     var yOffset = (this.canvas.height - targetHeight) / 2 + this.pan[1];
-    
+
     var tempcanvas = document.createElement("canvas");
     tempcanvas.height = this.currentFile.Rows;
     tempcanvas.width = this.currentFile.Columns;
     var tempContext = tempcanvas.getContext("2d");
-    
+
     tempContext.putImageData(imgData, 0, 0);
     this.context.drawImage(tempcanvas, xOffset, yOffset, targetWidth, targetHeight);
 };
--- js/DcmViewer.js
+++ js/DcmViewer.js
@@ -27,9 +27,37 @@ DcmViewer.prototype.loadFile = function(rawFile) {
             var array = new Uint8Array(evt.target.result);
             var parser = new DicomParser(array);
             var file = parser.parse_file();
-            
-            // TODO: Hier noch überprüfung auf RescaleSlope, Intercept, ww & wc und ggf setzen
-            
+
+            console.log(file);
+            var str = '';
+
+            // TODO: Hier noch überprüfung auf RescaleSlope, Intercept, ww & wc setzen
+            if(typeof file.RescaleSlope === 'undefined') {
+                file.RescaleSlope = 1;
+                str += 'RescaleSlope undefined, ';
+            }
+            if(typeof file.RescaleIntercept === 'undefined') {
+                file.RescaleIntercept = 0;
+                str += 'RescaleIntercept undefined, ';
+            }
+            if(typeof file.WindowCenter === 'undefined') {
+               file.WindowCenter = 85;
+                str += 'WindowCenter undefined ';
+            } 
+            if(typeof file.WindowWidth === 'undefined') {
+               file.WindowWidth = 171;
+               str += 'WindowWidth undefined, ';
+            }
+            if($.isArray(file.WindowCenter)) {
+                file.WindowCenter = file.WindowCenter[0];
+                str += 'WindowCenter isArray, ';
+            }
+            if($.isArray(file.WindowWidth)) {
+                file.WindowWidth = file.WindowWidth[0];
+                str += 'WindowWidth isArray ';
+            }
+            str.length > 0 ? console.log(str) : str;
+
             tmpPainter.setFile(file);
             tmpPainter.drawImg();
         }
