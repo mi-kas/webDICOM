a8220f9dbe346616582f8b7c9cd57ca7d8a0be78
mikas <michael.kaserer@hotmail.com>
2013-03-16 17:27:01 +0100

    Scroll events normalized between different browsers.

--- js/DcmViewer.js
+++ js/DcmViewer.js
@@ -5,6 +5,7 @@ function DcmViewer(canvasId) {
     this.painter = new CanvasPainter(this.canvasId);
     this.toolbox = new Toolbox(this.painter);
     this.scrollIndex = 0;
+    this.eventsEnabled = false;
 }
 
 DcmViewer.prototype.setCurrentTool = function(toolName) {
@@ -29,7 +30,7 @@ DcmViewer.prototype.loadFile = function(rawFile, index, end) {
             var file = parser.parse_file();
             _this.files.push(file);
             var str = '';
-            console.log(file);
+
             file.Scale = 1;
             file.Pan = [0, 0];
 
@@ -62,41 +63,36 @@ DcmViewer.prototype.loadFile = function(rawFile, index, end) {
             if(index === end - 1) {
                 _this.painter.setFile(file);
                 _this.painter.drawImg();
+                _this.eventsEnabled = true;
             }
         }
     };
 };
 
 DcmViewer.prototype.eventHandler = function(e) {
-    // firefox doesn't have offsetX
-    e.x = !e.offsetX ? (e.pageX - $(e.target).offset().left) : e.offsetX;
-    e.y = !e.offsetY ? (e.pageY - $(e.target).offset().top) : e.offsetY;
+    if(this.eventsEnabled) {
+        // Firefox doesn't have the offsetX/offsetY properties -> own calculation
+        e.x = !e.offsetX ? (e.pageX - $(e.target).offset().left) : e.offsetX;
+        e.y = !e.offsetY ? (e.pageY - $(e.target).offset().top) : e.offsetY;
 
-    // pass the event to the currentTool of the toolbox
-    var eventFunc = this.toolbox.currentTool[e.type];
-    if(eventFunc) {
-        eventFunc(e.x, e.y, this.painter);
+        // pass the event to the currentTool of the toolbox
+        var eventFunc = this.toolbox.currentTool[e.type];
+        if(eventFunc) {
+            eventFunc(e.x, e.y, this.painter);
+        }
     }
 };
 
 DcmViewer.prototype.scrollHandler = function(evt) {
-    if(this.files.length > 1) {
+    if(this.files.length > 1 && this.eventsEnabled) {
         evt.preventDefault();
         var e = evt.originalEvent;
 
-        if(e.detail) {
-            // Firefox
-            var delta = e.detail * (-1000);
-            this.scrollIndex = (delta <= -1000) ? this.scrollIndex + 1 : this.scrollIndex - 1;
-        } else {
-            // Non firefox browsers
-            var delta = e.wheelDelta;
-            if(delta > 0) {
-                this.scrollIndex = (delta > 25) ? this.scrollIndex + 1 : this.scrollIndex;
-            } else {
-                this.scrollIndex = (delta < -25) ? this.scrollIndex - 1 : this.scrollIndex;
-            }
-        }
+        // Firefox uses detail. Chrome and Safari wheelDelta. Normalizing the different units.
+        var delta = e.detail ? -e.detail : e.wheelDelta / 3.0;
+        this.scrollIndex = (delta >= 1) ? this.scrollIndex + 1 : (delta <= -1) ? this.scrollIndex - 1 : this.scrollIndex;
+
+        // cyclic scrolling
         this.scrollIndex = (this.scrollIndex < 0) ? this.files.length - 1 : (this.scrollIndex > this.files.length - 1) ? 0 : this.scrollIndex;
 
         // save modified values (wc, ww, pan, scale) of the image
