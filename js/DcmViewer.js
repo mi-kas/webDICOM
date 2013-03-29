a8c573c4423652e34f6b1cbb746b6e4da67b829c
mikas <migge36@web.de>
2013-03-29 18:08:48 +0100

    Update patient info started. Needs to be improved.

--- js/DcmViewer.js
+++ js/DcmViewer.js
@@ -18,7 +18,8 @@ DcmViewer.prototype.setParsedFiles = function(files) {
     this.painter.drawImg();
     this.eventsEnabled = true;
     var self = this;
-    updateInfo();
+    clearInfo();
+    updateInfo(this.painter);
     $("#slider").slider({
         value: 0,
         min: 0,
@@ -68,15 +69,42 @@ DcmViewer.prototype.scrollOne = function(num) {
     this.painter.drawImg();
 };
 
-// TODO: überprüfen auf undefined
-updateInfo = function(_this) {
-    $('#patientsName').text(_this.currentFile.PatientsName + ' (' + _this.currentFile.PatientsSex + ') ' + _this.currentFile.PatientID);
+var updateInfo = function(_this) {
+    var pName = _this.currentFile.PatientsName ? _this.currentFile.PatientsName : ' - ';
+    var pSex = _this.currentFile.PatientsSex ? ' (' + _this.currentFile.PatientsSex + ') ' : ' ';
+    var pID = _this.currentFile.PatientID ? _this.currentFile.PatientID : ' - ';
     var x = _this.currentFile.PatientsBirthDate;
-    $('#age').text(new Date(x.slice(6) + "/" + x.slice(4, 6) + "/" + x.slice(0, 4)).toLocaleDateString());
-    $('#wCenter').text(_this.wc.toFixed(0));
-    $('#wWidth').text(_this.ww.toFixed(0));
+    var pDate = '';
+    
+    if(x) {
+        // TODO: check if valid date.
+        if(new Date(x.slice(0, 4) + "/" + x.slice(4, 6) + "/" + x.slice(6, 8))) {
+            pDate = new Date(x.slice(0, 4) + "/" + x.slice(4, 6) + "/" + x.slice(6, 8)).toLocaleDateString();
+            if(_this.currentFile.PatientsAge) {
+                pDate += '  ' + _this.currentFile.PatientsAge;
+            }
+        }
+    }
+
     x = _this.currentFile.SeriesDate;
     var time = _this.currentFile.SeriesTime;
-    $('#studyDate').text(new Date(x.slice(6) + "/" + x.slice(4, 6) + "/" + x.slice(0, 4)).toLocaleDateString() + '  ' + time.slice(0, 2) + ':' + time.slice(2, 4) + ':' + time.slice(4, 6));
-    $('#studyDescription').text(_this.currentFile.StudyDescription);
+    var sDate = x ? new Date(x.slice(0, 4) + "/" + x.slice(4, 6) + "/" + x.slice(6, 8)).toLocaleDateString() : ' - ';
+    sDate += time ? '  ' + time.slice(0, 2) + ':' + time.slice(2, 4) + ':' + time.slice(4, 6) : '';
+    var sDesc = _this.currentFile.StudyDescription ? _this.currentFile.StudyDescription : ' - ';
+
+    $('#patientsName').text(pName + pSex + pID);
+    $('#age').text(pDate);
+    $('#wCenter').text(_this.wc.toFixed(0));
+    $('#wWidth').text(_this.ww.toFixed(0));
+    $('#studyDate').text(sDate);
+    $('#studyDescription').text(sDesc);
+};
+
+var clearInfo = function() {
+    $('#patientsName').text('');
+    $('#age').text('');
+    $('#wCenter').text('');
+    $('#wWidth').text('');
+    $('#studyDate').text('');
+    $('#studyDescription').text('');
 };
