8defb2a7fdbc1317e27a7674282c1bc05dba5fa6
mikas <migge36@web.de>
2013-04-30 14:13:03 +0200

    Input name changed.

--- js/libs/jQuery-UI-FileInput/js/fileinput.jquery.js
+++ js/libs/jQuery-UI-FileInput/js/fileinput.jquery.js
@@ -107,7 +107,15 @@
             }
             //get folder name
             var tmp = this.$element.context.files[0].webkitRelativePath;
-            var fileName = tmp ? tmp.split('/')[0] : this.$element.val().split(/\\/).pop();
+            var fileName;
+            var length = this.$element.context.files.length;
+            if(tmp) {
+                fileName = tmp.split('/')[0];
+                fileName += length > 1 ? ' (' + length + ' files)': ' (' + length + ' file)';
+            } else {
+                fileName = length > 1 ? length + ' files': length + ' file'; //this.$element.val().split(/\\/).pop();
+            }
+            
             if (!fileName) {
                 this.$uploadFeedback
                     .val(this.options.feedback_text) //set feedback text to filename
